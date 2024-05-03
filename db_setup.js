30ea27eb4432d2f62bec4d5dd90a223ac81c35e8
selvakum <mathusan.selvakumar@etu.u-paris.fr>
2024-05-03 18:41:02 +0200

    format

--- db_setup.js
+++ db_setup.js
@@ -26,24 +26,30 @@ pool.connect((err, client, done) => {
 });
 
 async function createAdminUser(db) {
-    const username = "admin";
-    const password = "admin";
-    const role = "manager";
-    const hashedPassword = await bcrypt.hash(password, 10); // Hash the password
-  
-    try {
-      // Check if the admin user already exists
-      const res = await db.query("SELECT * FROM loyalty_card.users WHERE username = $1", [username]);
-      if (res.rows.length === 0) {
-        // Insert the new admin user if not existing
-        await db.query("INSERT INTO loyalty_card.users (username, password, role) VALUES ($1, $2, $3)", [username, hashedPassword, role]);
-        console.log("Admin user created successfully");
-      } else {
-        console.log("Admin user already exists");
-      }
-    } catch (err) {
-      console.error("Failed to create admin user:", err);
+  const username = "admin";
+  const password = "admin";
+  const role = "manager";
+  const hashedPassword = await bcrypt.hash(password, 10); // Hash the password
+
+  try {
+    // Check if the admin user already exists
+    const res = await db.query(
+      "SELECT * FROM loyalty_card.users WHERE username = $1",
+      [username]
+    );
+    if (res.rows.length === 0) {
+      // Insert the new admin user if not existing
+      await db.query(
+        "INSERT INTO loyalty_card.users (username, password, role) VALUES ($1, $2, $3)",
+        [username, hashedPassword, role]
+      );
+      console.log("Admin user created successfully");
+    } else {
+      console.log("Admin user already exists");
     }
+  } catch (err) {
+    console.error("Failed to create admin user:", err);
   }
+}
 
 createAdminUser(pool); // Call the function to create the admin user
