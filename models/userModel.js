0df9a441d121d0b9c58702acabc62109931f786e
selvakum <mathusan.selvakumar@etu.u-paris.fr>
2024-05-04 15:47:14 +0200

    fix: bycrptjs

--- models/userModel.js
+++ models/userModel.js
@@ -1,4 +1,4 @@
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const ClientModel = require("../models/clientModel");
 const ManagerModel = require("../models/managerModel");
 
