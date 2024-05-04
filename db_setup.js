b7eac41605f6205d65d9d81ce2313c59ea94de69
selvakum <mathusan.selvakumar@etu.u-paris.fr>
2024-05-04 16:52:00 +0200

    feat: better admin roles

--- db_setup.js
+++ db_setup.js
@@ -6,27 +6,44 @@ const pool = require("./models/db");
 const sqlFilePath = path.join(__dirname, "init_db.sql");
 const sql = fs.readFileSync(sqlFilePath, { encoding: "UTF-8" });
 
-// Function to create admin user
-async function createAdminUser(db) {
-  const email = "admin@example.com";
-  const password = "admin";
-  const firstName = "Admin";
-  const lastName = "User";
-  const hashedPassword = await bcrypt.hash(password, 10); // Hash the password
+const adminList = [
+  {
+    email: "rtheo@mail.com",
+    password: "raoul",
+    firstName: "Theo",
+    lastName: "Raoul",
+  },
+  {
+    email: "smathusan@mail.com",
+    password: "mathusan",
+    firstName: "Mathusan",
+    lastName: "Selvakumar",
+  }
+];
 
+// Function to create admin user
+async function createAdminUser(db, adminList) {
   try {
-    const res = await db.query(
-      "SELECT * FROM loyalty_card.managers WHERE email = $1",
-      [email],
-    );
-    if (res.rows.length === 0) {
-      await db.query(
-        "INSERT INTO loyalty_card.managers (first_name, last_name, email, password) VALUES ($1, $2, $3, $4)",
-        [firstName, lastName, email, hashedPassword],
+    for (let i = 0; i < adminList.length; i++) {
+      const email = adminList[i].email;
+      const password = adminList[i].password;
+      const firstName = adminList[i].firstName;
+      const lastName = adminList[i].lastName;
+      const hashedPassword = await bcrypt.hash(password, 10); // Hash the password
+
+      const res = await db.query(
+        "SELECT * FROM loyalty_card.managers WHERE email = $1",
+        [email],
       );
-      console.log("Admin user created successfully");
-    } else {
-      console.log("Admin user already exists");
+      if (res.rows.length === 0) {
+        await db.query(
+          "INSERT INTO loyalty_card.managers (first_name, last_name, email, password) VALUES ($1, $2, $3, $4)",
+          [firstName, lastName, email, hashedPassword],
+        );
+        console.log("Admin user created:", email);
+      } else {
+        console.log("Admin user already exists:", email);
+      }
     }
   } catch (err) {
     console.error("Failed to create admin user:", err);
@@ -47,7 +64,8 @@ pool.connect((err, client, done) => {
       pool.end(); // Close the pool if there is an error
     } else {
       console.log("Database has been successfully initialized");
-      await createAdminUser(pool);
+      await createAdminUser(pool, adminList);
+
     }
   });
 });
